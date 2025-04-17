a7efb3f050b13571031a042138ef8f7043f5de6c
Otávio Bresolin de Oliveira <otaviobresolin@MacBook-Air-de-Otavio.local>
2025-04-17 11:33:51 -0300

    Corrigido erro de importação e estrutura da HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,10 @@
-import Home from '../Otavio Site Home';
-export default Home;
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Bem-vindo ao site Bresolin!</h1>
+    </Layout>
+  );
+}

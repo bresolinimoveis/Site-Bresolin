9cd1e98341c3aca424975067ccc110afa2330d16
Otávio Bresolin de Oliveira <otaviobresolin@MacBook-Air-de-Otavio.local>
2025-04-17 13:57:43 -0300

    adicionando menu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div style={{ padding: '20px' }}>
+      <nav style={{ marginBottom: '20px' }}>
+        <Link href="/">Início</Link> |{" "}
+        <Link href="/sobre">Sobre</Link> |{" "}
+        <Link href="/imoveis">Imóveis</Link> |{" "}
+        <Link href="/contato">Contato</Link>
+      </nav>
       {children}
     </div>
   );
